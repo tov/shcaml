bf575bcbc93e288a809577d56ef74199fed9cb1a
Armael <armael@isomorphis.me>
2015-12-22 19:23:48 +0100

    Add some info that was in the original configure.ac

--- pkg/config.ml
+++ pkg/config.ml
@@ -13,5 +13,18 @@ module Config = struct
     [ "NAME", "shcaml";
       "VERSION", "0.1.3";
       "CODENAME", "Shmeer";
+
+      (* shlock shmaltz shadken shanda shaygets shayner shiksa
+         shlemiel shlep shmendrick shmegegge
+         shmutzik shnorrer shtik shtetl shtunk
+         shvitz
+
+         Used:
+         0.1.0 shmatta
+         0.1.1 shmooz
+         0.1.2 shlimazl
+         0.1.3 shmeer
+       *)
+
       "BUILDTIME", time; ]
 end
